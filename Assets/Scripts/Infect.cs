9f87e9feea37181a2fb8814160e400d9802c2fd0
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2022-06-06 16:50:04 +0300

    tartutettava gameobject

---
+++ Assets/Scripts/Infect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infect : MonoBehaviour
+{
+    SpriteRenderer m_SpriteRenderer;
+    Color m_NewColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            m_NewColor = new Color(0, 1, 0, 1);
+            m_SpriteRenderer.color = m_NewColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
