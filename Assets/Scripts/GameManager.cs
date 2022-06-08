e838e324d4e4a526f1c1013662faba1f8b7c4a9e
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2022-06-08 17:59:21 +0300

    simple score system (needs improving)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /* A static variable that is accessible from anywhere in the code. */
+    public static GameManager Instance;
+
+    public float score { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Random.Range(250, 501) * Time.deltaTime;
+
+        score = Mathf.Round(score);
+        Debug.Log(score);
+    }
+}
